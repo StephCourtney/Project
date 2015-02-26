a702e4d5464f02ec17e679eb298c95716e916d6e
StephCourtney <stephcourtney28@gmail.com>
2015-02-26 10:41:02 +0000

    Fixed

---
+++ AzureMediaPortal/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureMediaPortal.Models {
+    public class Comment {
+        public int CommentID { get; set; }
+
+        public string CommentText { get; set; }
+
+        public System.DateTime CommentTime { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+    }
+}
